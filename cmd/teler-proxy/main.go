90e125236ac7d39103a926ac99872ed10e55df6f
Dwi Siswanto <me@dw1.io>
2023-07-10 04:36:15 +0700

    feat(cmd): print detailed err

--- cmd/teler-proxy/main.go
+++ cmd/teler-proxy/main.go
@@ -9,10 +9,10 @@ func main() {
 	opt := runner.ParseOptions()
 
 	if err := opt.Validate(); err != nil {
-		log.Fatal("Cannot validate options", "message", err)
+		log.Fatal("Cannot validate options", "msg", err)
 	}
 
 	if err := runner.New(opt); err != nil {
-		log.Fatal(err)
+		log.Fatal("Something went wrong", "err", err)
 	}
 }
