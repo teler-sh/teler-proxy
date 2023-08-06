b6292be71398332cfad43c82c938ff416d37a7b2
Dwi Siswanto <me@dw1.io>
2023-08-06 18:29:12 +0700

    test(common): add tests for validator

---
+++ common/validator_test.go
@@ -0,0 +1,77 @@
+package common
+
+import "testing"
+
+func TestOptions_Validate_ValidConfig(t *testing.T) {
+	opt := &Options{
+		Port:        8080,
+		Destination: "example.com",
+		Config: &Config{
+			Path:   "config.yaml",
+			Format: "yaml",
+		},
+		TLS: nil,
+	}
+
+	err := opt.Validate()
+	if err != nil {
+		t.Errorf("Expected no error, but got: %v", err)
+	}
+}
+
+func TestOptions_Validate_EmptyDestination(t *testing.T) {
+	opt := &Options{
+		Port:        8080,
+		Destination: "",
+		Config: &Config{
+			Path:   "config.yaml",
+			Format: "yaml",
+		},
+		TLS: nil,
+	}
+
+	err := opt.Validate()
+	if err != ErrDestAddressEmpty {
+		t.Errorf("Expected ErrDestAddressEmpty, but got: %v", err)
+	}
+}
+
+func TestOptions_Validate_InvalidConfigFormat(t *testing.T) {
+	opt := &Options{
+		Port:        8080,
+		Destination: "example.com",
+		Config: &Config{
+			Path:   "config.json",
+			Format: "xml",
+		},
+		TLS: nil,
+	}
+
+	err := opt.Validate()
+	if err != ErrCfgFileFormatInv {
+		t.Errorf("Expected ErrCfgFileFormatInv, but got: %v", err)
+	}
+}
+
+func TestOptions_Validate_MissingConfigPathAndFormat(t *testing.T) {
+	opt := &Options{
+		Port:        8080,
+		Destination: "example.com",
+		Config:      &Config{},
+		TLS:         nil,
+	}
+
+	err := opt.Validate()
+	if err != nil {
+		t.Errorf("Expected no error, but got: %v", err)
+	}
+}
+
+func TestOptions_Validate_NilOptions(t *testing.T) {
+	opt := &Options{}
+
+	err := opt.Validate()
+	if err != ErrDestAddressEmpty {
+		t.Errorf("Expected ErrDestAddressEmpty, but got: %v", err)
+	}
+}
