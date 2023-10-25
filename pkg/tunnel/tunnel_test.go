0351fbfa24c48bfa79cc3f6060b26b7a76fbae89
Dwi Siswanto <me@dw1.io>
2023-10-25 08:31:47 +0700

    feat(tunnel): add `writer` parameter
    Signed-off-by: Dwi Siswanto <me@dw1.io>

--- pkg/tunnel/tunnel_test.go
+++ pkg/tunnel/tunnel_test.go
@@ -37,7 +37,7 @@ func init() {
 
 func TestNewTunnel(t *testing.T) {
 	// Test case 1: valid destination and no configuration file
-	tun, err := NewTunnel(8080, dest, "", "")
+	tun, err := NewTunnel(8080, dest, "", "", nil)
 	if err != nil {
 		t.Fatalf("Expected no error, but got: %v", err)
 	}
@@ -47,20 +47,20 @@ func TestNewTunnel(t *testing.T) {
 	}
 
 	// Test case 2: invalid destination (empty)
-	_, err = NewTunnel(8080, "", "", "")
+	_, err = NewTunnel(8080, "", "", "", nil)
 	if err != common.ErrDestAddressEmpty {
 		t.Fatalf("Expected %v, but got: %v", common.ErrDestAddressEmpty, err)
 	}
 
 	// Test case 3: with config file but empty format
-	_, err = NewTunnel(8080, dest, filepath.Join(workspaceDir, "teler-waf.conf.example.yaml"), "")
+	_, err = NewTunnel(8080, dest, filepath.Join(workspaceDir, "teler-waf.conf.example.yaml"), "", nil)
 	if err != common.ErrCfgFileFormatUnd {
 		t.Fatalf("Expected %v, but got: %v", common.ErrCfgFileFormatUnd, err)
 	}
 
 	// Test case 4: with config file and YAML format
 	tun = nil
-	tun, err = NewTunnel(8080, dest, filepath.Join(workspaceDir, "teler-waf.conf.example.yaml"), "yaml")
+	tun, err = NewTunnel(8080, dest, filepath.Join(workspaceDir, "teler-waf.conf.example.yaml"), "yaml", nil)
 	if err != nil {
 		t.Fatalf("Expected no error, but got: %v", err)
 	}
@@ -71,7 +71,7 @@ func TestNewTunnel(t *testing.T) {
 
 	// Test case 5: with config file and JSON format
 	tun = nil
-	tun, err = NewTunnel(8080, dest, filepath.Join(workspaceDir, "teler-waf.conf.example.json"), "json")
+	tun, err = NewTunnel(8080, dest, filepath.Join(workspaceDir, "teler-waf.conf.example.json"), "json", nil)
 	if err != nil {
 		t.Fatalf("Expected no error, but got: %v", err)
 	}
@@ -81,26 +81,32 @@ func TestNewTunnel(t *testing.T) {
 	}
 
 	// Test case 6: with config file and xml format
-	_, err = NewTunnel(8080, dest, filepath.Join(workspaceDir, "teler-waf.conf.example.json"), "xml")
+	_, err = NewTunnel(8080, dest, filepath.Join(workspaceDir, "teler-waf.conf.example.json"), "xml", nil)
 	if err != common.ErrCfgFileFormatInv {
 		t.Fatalf("Expected %v, but got: %v", common.ErrCfgFileFormatInv, err)
 	}
 
 	// Test case 7: invalid destination
 	tun = nil
-	tun, _ = NewTunnel(8080, "http://this is not a valid URL", "", "")
+	tun, _ = NewTunnel(8080, "http://this is not a valid URL", "", "", nil)
 	if tun != nil {
 		t.Fatalf("Expected %v, but got: %v", nil, tun)
 	}
 
 	// Test case 8: with invalid config file
-	_, err = NewTunnel(8080, dest, "nonexistent", "yaml")
+	_, err = NewTunnel(8080, dest, "nonexistent", "yaml", nil)
 	if err == nil {
 		t.Fatal("Expected error, but got nil")
 	}
 
 	// Test case 9: with invalid config file
-	_, err = NewTunnel(8080, dest, "nonexistent", "json")
+	_, err = NewTunnel(8080, dest, "nonexistent", "json", nil)
+	if err == nil {
+		t.Fatal("Expected no error, but got nil")
+	}
+
+	// Test case 10: with io.Writer
+	_, err = NewTunnel(8080, dest, "nonexistent", "json", os.Stderr)
 	if err == nil {
 		t.Fatal("Expected no error, but got nil")
 	}
@@ -138,7 +144,7 @@ func BenchmarkNewTunnel(b *testing.B) {
 		b.ReportAllocs()
 
 		for i := 0; i < b.N; i++ {
-			_, err := NewTunnel(8080, dest, filepath.Join(workspaceDir, "teler-waf.conf.example.yaml"), "yaml")
+			_, err := NewTunnel(8080, dest, filepath.Join(workspaceDir, "teler-waf.conf.example.yaml"), "yaml", nil)
 			if err != nil {
 				b.Fatalf("Expected no error, but got: %v", err)
 			}
@@ -150,7 +156,7 @@ func BenchmarkNewTunnel(b *testing.B) {
 		b.ReportAllocs()
 
 		for i := 0; i < b.N; i++ {
-			_, err := NewTunnel(8080, dest, filepath.Join(workspaceDir, "teler-waf.conf.example.json"), "json")
+			_, err := NewTunnel(8080, dest, filepath.Join(workspaceDir, "teler-waf.conf.example.json"), "json", nil)
 			if err != nil {
 				b.Fatalf("Expected no error, but got: %v", err)
 			}
