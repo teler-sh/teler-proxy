fb520c6a7554cf1be2a6c511acad4bd61941ba32
Dwi Siswanto <me@dw1.io>
2023-10-25 08:31:47 +0700

    feat(tunnel): add `writer` parameter
    Signed-off-by: Dwi Siswanto <me@dw1.io>

--- pkg/tunnel/tunnel.go
+++ pkg/tunnel/tunnel.go
@@ -1,6 +1,25 @@
+/*
+Package tunnel provides functionality for creating HTTP tunnels
+and reverse proxies with [teler] WAF capabilities.
+
+The main components of this package include the [Tunnel] type,
+which represents a tunneling configuration, and related functions
+and methods for tunnel setup and HTTP request handling.
+
+To create a new tunnel, use the [NewTunnel] function, specifying
+the local port, destination address, and optional configuration
+parameters. The [Tunnel] type also provides the [Tunnel.ServeHTTP] method
+for handling incoming HTTP requests and proxying them to the
+destination, analyzing the incoming HTTP request from threats using
+the [teler.Teler] middleware.
+
+Additional configuration options can be loaded from YAML or JSON
+files, allowing for customizing the [teler] WAF behavior.
+*/
 package tunnel
 
 import (
+	"io"
 	"strings"
 
 	"net/http"
@@ -19,11 +38,26 @@ type Tunnel struct {
 	Options teler.Options
 }
 
-func NewTunnel(port int, dest, cfgPath, optFormat string) (*Tunnel, error) {
+// NewTunnel creates a new [Tunnel] instance for proxying HTTP traffic.
+//
+// Parameters:
+//   - `port`: The local port on which the tunnel will listen for incoming requests.
+//   - `dest`: The destination address to which incoming requests will be forwarded.
+//   - `cfgPath`: The path to a configuration file for additional tunnel options.
+//   - `optFormat`: The format of the configuration file ("yaml" or "json").
+//   - `writer`: An optional [io.Writer] where tunnel log output will be written.
+//     Pass nil to use default [teler] logging only.
+//
+// Please be aware that when you pass a custom `writer`, the [teler.Options.NoStderr]
+// option value will be forcibly set to `true`, regardless of the `no_stderr` value
+// that might be loaded from additional configuration options.
+func NewTunnel(port int, dest, cfgPath, optFormat string, writer io.Writer) (*Tunnel, error) {
 	if dest == "" {
 		return nil, common.ErrDestAddressEmpty
 	}
 
+	// NOTE(dwisiswant0): should we accept the input `dest` parameter
+	// as pointer of url.URL directly instead of string?
 	destURL, err := url.Parse(dest)
 	if err != nil {
 		return nil, err
@@ -51,14 +85,25 @@ func NewTunnel(port int, dest, cfgPath, optFormat string) (*Tunnel, error) {
 		}
 
 		tun.Options = opt
-		tun.Teler = teler.New(opt)
-	} else {
-		tun.Teler = teler.New()
 	}
+
+	if writer != nil {
+		opt.LogWriter = writer
+		opt.NoStderr = true
+	}
+
+	tun.Teler = teler.New(opt)
 
 	return tun, nil
 }
 
+// ServeHTTP is a method of the [Tunnel] type, which allows
+// the [Tunnel] to implement the [http.Handler] interface.
+//
+// This method forwards the incoming HTTP request to the
+// [httputil.ReverseProxy.ServeHTTP] method, while also
+// analyzing the incoming HTTP request from threats using
+// the [teler.Teler] middleware.
 func (t *Tunnel) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.Teler.HandlerFuncWithNext(w, r, t.ReverseProxy.ServeHTTP)
 }
